49d4036fb66b6d5940c78e48e0b29aab1ba307ca
Alex Shapovalov <evtiel.go@gmail.com>
2020-08-11 16:00:09 -0400

    Added fuction to add new post by click on button using Context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+    const [ blogPost, setBlogPost ] = useState([]);
+
+    const addBlogPost = () => {
+        setBlogPost([...blogPost, { title: `Blog Post #${blogPost.length + 1}` }])
+    }
+
     return (
-        <BlogContext.Provider>
+        <BlogContext.Provider value={{ data: blogPost, addBlogPost: addBlogPost }}>
             {children}
         </BlogContext.Provider>
     )
 };
+
+export default BlogContext;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet, Text, FlatList, Button } from 'react-native';
+import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
+    const { data, addBlogPost } = useContext(BlogContext);
+
     return (
         <View>
             <Text>Index Screen</Text>
+            <Button title="Add Post" onPress={() => addBlogPost()}/>
+            <FlatList 
+                data={data}
+                keyExtractor={(blogPost) => blogPost.title}
+                renderItem={({ item }) => {
+                    return <Text>{item.title}</Text>
+                }}
+            />
         </View>
     )
 };
