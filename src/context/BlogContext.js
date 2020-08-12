4321c2d8d82e201940987ed4c2ddf10d6bc554ba
Alex Shapovalov <evtiel.go@gmail.com>
2020-08-12 07:53:45 -0400

    switch to useResucer for future updates

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, { title: `Blog Post #${state.length + 1 }` }]
+        default:
+            return state
+    }
+};
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-    const [ blogPost, setBlogPost ] = useState([]);
+    const [ blogPost, dispatch ] = useReducer(blogReducer, []);
 
     const addBlogPost = () => {
-        setBlogPost([...blogPost, { title: `Blog Post #${blogPost.length + 1}` }])
-    }
+        dispatch({ type: 'add_blogpost' })
+    };
 
     return (
         <BlogContext.Provider value={{ data: blogPost, addBlogPost: addBlogPost }}>
